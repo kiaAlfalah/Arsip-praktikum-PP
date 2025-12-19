68cf9928dd27b693b51ad0f8fd3f3948641042ae
kiaAlfalah <100498730+kiaAlfalah@users.noreply.github.com>
2025-12-20 00:56:55 +0700

    Add basic C++ program for introduction

---
+++ Prak1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string nama = "Irhab Bagas Dwi Prabowo";
+  string NPM = "2517051065";
+  
+  cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+  cout << "Nama saya adalah " << nama << endl;
+  cout << "NPM saya adalah" << NPM << endl << endl;
+  
+  cout << "Unila Indah" << endl;
+  cout << "Be Strong" << endl;
+}
