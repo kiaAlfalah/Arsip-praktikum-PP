50afecef509fdfced9a9c6016f8abcf97b694c94
kiaAlfalah <100498730+kiaAlfalah@users.noreply.github.com>
2025-12-20 00:57:37 +0700

    Add basic C++ program for addition

---
+++ prak2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 2;
+  int b = 2;
+  int c = a + b;
+  
+  cout << "Nilai a : " << a << endl;
+  cout << "Nilai b : " << b << endl;
+  cout << "Nilai c : " << c << endl;
+  
+  a = a + b; // sama dengan a += b
+  
+  cout << "NIlai a setelah (a= a + b) : " << a << endl;
+  
+  return 0;
+}
